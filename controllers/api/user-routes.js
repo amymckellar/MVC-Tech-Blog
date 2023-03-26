ac31b35b71c79d0c9a17b4772561a8af8fcc3d3f
amymckellar <amy_mckellar@yahoo.com>
2023-03-25 21:28:49 -0500

    adding GET a single user in the user-routes.js file

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,3 +13,38 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// This will GET a single user
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "post_url", "created_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
