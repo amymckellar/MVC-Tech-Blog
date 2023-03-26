9835011d79e792107e1351339be97ca5f5ac31ac
amymckellar <amy_mckellar@yahoo.com>
2023-03-25 21:03:27 -0500

    adding the function to GET all comments in the comment-routes.js file

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+// This will GET all comments
+router.get("/", (req, res) => {
+  Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
