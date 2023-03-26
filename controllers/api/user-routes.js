d45ca4df9cb72697218ffa1c1dd8a9bac8b810d0
amymckellar <amy_mckellar@yahoo.com>
2023-03-25 21:24:21 -0500

    adding GET all users in the user-routes.js file

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,15 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+// This will GET all users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
