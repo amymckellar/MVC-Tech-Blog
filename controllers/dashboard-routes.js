0ce52c720fb1c4f7e9ef0378f071eba9f6377fe5
amymckellar <amy_mckellar@yahoo.com>
2023-03-25 21:51:20 -0500

    adding GET post for dashboard in the dashboard-routes.js file

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,47 @@ const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 const sequelize = require("../../config/connection");
+
+// GET all posts for dashboard
+router.get("/", withAuth, (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
