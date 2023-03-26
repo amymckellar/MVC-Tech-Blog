72d40a6d939eb7f2637ad6e74d3e1da5857d3440
amymckellar <amy_mckellar@yahoo.com>
2023-03-25 21:20:07 -0500

    adding an updated post in the post-routes.js file in the api folder

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -77,3 +77,28 @@ router.post("/", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// This will PUT an updated post
+router.put("/:id", withAuth, (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
