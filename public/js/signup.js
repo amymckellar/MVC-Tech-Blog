d8f25133ee17aa0293426eeb5c3f939a3768e294
amymckellar <amy_mckellar@yahoo.com>
2023-03-26 10:34:59 -0500

    adding code in the comment-seeds.json file

---
+++ public/js/signup.js
@@ -0,0 +1,31 @@
+// This is the function to signup a new user
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
