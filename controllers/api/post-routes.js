b496155aa3dfc8e5d9e38fdf756d732fa9e5964c
amymckellar <amy_mckellar@yahoo.com>
2023-03-25 21:16:13 -0500

    adding GET all posts in the post-routes.js file in the api folder

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,30 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+// This will GET all posts
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "post_url", "title", "created_at"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
