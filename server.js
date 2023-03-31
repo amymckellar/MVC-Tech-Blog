eb132e0ebd818f28f0c3e1eb07bcc214288fcada
amymckellar <amy_mckellar@yahoo.com>
2023-03-31 13:24:05 -0500

    adding a const sess in the server.js file

--- server.js
+++ server.js
@@ -14,3 +14,13 @@ const hbs = exphbs.create({ helpers });
 const session = require("express-session");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
